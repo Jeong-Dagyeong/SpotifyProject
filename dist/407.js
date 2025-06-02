510905b4b3301299cec2618408c9d2c3a9d7daec
dagyeong Jeong <jdk09140@gmail.com>
2025-06-02 22:16:05 +0900

    feat: _redirects설정

--- dist/407.js
+++ dist/407.js
@@ -1,2 +1,2 @@
 "use strict";(self.webpackChunkspotify_demo=self.webpackChunkspotify_demo||[]).push([[407],{407:(e,s,d)=>{d.r(s),d.d(s,{default:()=>t}),d(540);var o=d(848);const t=()=>(0,o.jsx)("div",{children:"HomePage"})}}]);
-//# sourceMappingURL=407.js.map?ver=2604443fd3772764fada
+//# sourceMappingURL=407.js.map?ver=45b7934639e094aaad95
