fb5e6988e68090d2b240a1a836572aaab7d8459b
dagyeong Jeong <jdk09140@gmail.com>
2025-06-02 21:38:21 +0900

    webpack, babel, react-router설치 및 초기셋팅

---
+++ src/common/components/LoadingPage.tsx
@@ -0,0 +1,26 @@
+import React from 'react'
+
+const LoadingPage = () => {
+  return (
+    <div>
+      <div
+        style={{
+          display: 'flex',
+          justifyContent: 'center',
+          alignItems: 'center',
+          flexDirection: 'column',
+          height: '100vh',
+        }}
+      >
+        <div>
+          <p style={{ marginBottom: '20px', fontSize: '35px' }}>로딩 중입니다. 잠시만 기다려주세요...</p>
+        </div>
+        <div>
+          <img src="../../images/loadingImage1.gif" alt="로딩 이미지" style={{ width: '500px', height: '500px' }} />
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default LoadingPage
---
+++ webpack.config.js
@@ -0,0 +1,92 @@
+const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const Dotenv = require('dotenv-webpack')
+const { CleanWebpackPlugin } = require('clean-webpack-plugin')
+
+module.exports = (env, argv) => {
+  const isProduction = argv.mode === 'production'
+
+  return {
+    mode: isProduction ? 'production' : 'development',
+    entry: path.resolve(__dirname, 'src', 'index.tsx'), // 프로젝트가 시작되는 파일을 지정
+    resolve: {
+      extensions: ['.tsx', '.ts', '.js'],
+    },
+    module: {
+      rules: [
+        // 다양한 파일 형식을 처리하기 위한 규칙을 정의
+        {
+          test: /\.css$/i,
+          use: ['style-loader', 'css-loader'], // css 파일 처리
+        },
+        {
+          test: /\.svg$/,
+          use: [
+            {
+              loader: '@svgr/webpack',
+              options: {
+                icon: true,
+              },
+            },
+            {
+              loader: 'file-loader',
+              options: {
+                name: '[name].[ext]?ver=[hash]',
+                outputPath: 'images',
+              },
+            },
+          ],
+        },
+        {
+          test: /\.(png|jpg|gif|jpeg)$/,
+          use: [
+            {
+              loader: 'file-loader',
+              options: {
+                name: '[name].[ext]?ver=[hash]',
+                outputPath: 'images',
+              },
+            },
+          ],
+        },
+        {
+          test: /\.(ts|js)x?$/,
+          exclude: /node_modules/,
+          use: [
+            {
+              loader: 'babel-loader',
+            },
+          ],
+        },
+      ],
+    },
+    output: {
+      path: path.resolve(__dirname, 'dist'),
+      chunkFilename: '[name].js?ver=[hash]',
+      filename: '[name].js?ver=[hash]',
+      publicPath: '/',
+    },
+    devtool: isProduction ? 'source-map' : 'inline-source-map',
+    devServer: {
+      port: 3000,
+      open: true,
+      hot: true,
+      historyApiFallback: true,
+    },
+    optimization: {
+      minimize: isProduction,
+    },
+    plugins: [
+      // 빌드 프로세스를 도와주는 플러그인
+      new CleanWebpackPlugin(), // 이전 빌드 파일을 삭제
+      new HtmlWebpackPlugin({
+        // HTML 파일을 생성하고, 번들링된 자바스크립트를 자동으로 포함시킴
+        template: path.resolve(__dirname, 'src', 'index.html'), // 이거 만들어줘야함
+      }),
+      new Dotenv({
+        allowEmptyValues: true,
+        systemvars: true,
+      }),
+    ],
+  }
+}
