48ef30defc5cfb8c834afad75623446d5f5d9da7
dagyeong Jeong <jdk09140@gmail.com>
2025-06-08 00:57:15 +0900

    feat: spotify 프로필api

--- src/hooks/useExchangeToken.ts
+++ src/hooks/useExchangeToken.ts
@@ -1,12 +1,17 @@
-import { useMutation } from '@tanstack/react-query'
+import { useMutation, useQueryClient } from '@tanstack/react-query'
 import { ExchangeTokenResponse } from '../models/auth'
 import { exchangeToken } from '../apis/authApi'
 
 const useExchangeToken = () => {
+  const queryClient = useQueryClient()
+
   return useMutation<ExchangeTokenResponse, Error, { code: string; codeVerifier: string }>({
     mutationFn: ({ code, codeVerifier }) => exchangeToken(code, codeVerifier),
     onSuccess: (data) => {
       localStorage.setItem('access_token', data.access_token)
+      queryClient.invalidateQueries({
+        queryKey: ['current-user-profile'],
+      })
     },
   })
 }
--- src/layout/components/Navbar.tsx
+++ src/layout/components/Navbar.tsx
@@ -1,11 +1,29 @@
-import { Box } from '@mui/material'
-import React from 'react'
+import { Avatar, Box } from '@mui/material'
+import React, { useState } from 'react'
 import LoginBtn from '../../common/components/LoginBtn'
+import useGetCurrentUserProfile from '../../hooks/useGetCurrentUserProfile'
 
 const Navbar = () => {
+  const [imgError, setImgError] = useState(false)
+
+  const { data: userProfile } = useGetCurrentUserProfile()
+  const imageUrl = userProfile?.images?.[0]?.url
+
   return (
-    <Box display="flex" justifyContent="flex-end" alignItems="center" height="24px">
-      <LoginBtn />
+    // <Box display="flex" justifyContent="flex-end" alignItems="center" height="24px">
+    //   {userProfile ? userProfile?.images?.[0]?.url : <LoginBtn />}
+    // </Box>
+    <Box display="flex" justifyContent="flex-end" alignItems="center" height="40px">
+      {userProfile ? (
+        <Avatar
+          src={!imgError && imageUrl ? imageUrl : '/broken-image.jpg'}
+          onError={() => setImgError(true)}
+          alt="user-profile"
+          sx={{ width: 32, height: 32 }}
+        />
+      ) : (
+        <LoginBtn />
+      )}
     </Box>
   )
 }
